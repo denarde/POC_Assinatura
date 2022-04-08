b49fddfe96981b547961e2002470a9441d84f5a0
Luiz Gustavo Denarde dos Santos <36491514+denarde@users.noreply.github.com>
2022-04-08 11:33:22 -0300

    Consumindo Chaves do Certificado

--- src/main/java/Assinatura.java
+++ src/main/java/Assinatura.java
@@ -1,43 +1,69 @@
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.security.*;
 
 public class Assinatura {
 
-        private PublicKey pubKey;
-        private PrivateKey priKey;
+    private PublicKey pubKey;
+    private PrivateKey priKey;
 
-        public PublicKey getPubKey() {
-            return pubKey;
+    private char[] senha = "1234".toCharArray();
+
+    private KeyStore rep;
+    private String file = "src/main/java/certificado/WayneEnterprisesInc.pfx";
+
+    public PublicKey getPubKey() {
+        return pubKey;
+    }
+
+    public void setPubKey(PublicKey pubKey) {
+        this.pubKey = pubKey;
+    }
+
+    public PrivateKey getPriKey() {
+        return priKey;
+    }
+
+
+    public byte[] geraAssinatura(byte[] mensagem) throws Exception {
+
+        Signature sig = Signature.getInstance("MD5withRSA");
+
+        this.pubKey = getChavePublica();
+        PrivateKey priKey = getChavePrivada();
+
+        sig.initSign(priKey);
+
+        sig.update(mensagem);
+        byte[] assinatura = sig.sign();
+
+
+        return assinatura;
+
+    }
+
+    public PrivateKey getChavePrivada() throws Exception {
+        rep = KeyStore.getInstance("pkcs12");
+        InputStream entrada = new FileInputStream(file);
+        rep.load(entrada, senha);
+        entrada.close();
+
+        Key chavePrivada = (Key) rep.getKey("wayne enterprises, inc", senha);
+        if (chavePrivada instanceof PrivateKey) {
+            System.out.println("Chave Privada encontrada!");
+            return (PrivateKey) chavePrivada;
         }
+        return null;
+    }
 
-        public void setPubKey(PublicKey pubKey) {
-            this.pubKey = pubKey;
-        }
-
-        public PrivateKey getPriKey() {
-            return priKey;
-        }
-
-
-        public byte[] geraAssinatura(byte[] mensagem) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
-
-            Signature sig = Signature.getInstance("DSA");
-
-            KeyPairGenerator kpg = KeyPairGenerator.getInstance("DSA");
-
-            SecureRandom secRan = new SecureRandom();
-            kpg.initialize(512, secRan);
-            KeyPair keyP = kpg.generateKeyPair();
-
-            this.pubKey = keyP.getPublic();
-            PrivateKey priKey = keyP.getPrivate();
-
-            sig.initSign(priKey);
-
-            sig.update(mensagem);
-            byte[] assinatura = sig.sign();
-
-            return assinatura;
-
-        }
+    public PublicKey getChavePublica() throws Exception {
+        rep = KeyStore.getInstance("pkcs12");
+        InputStream entrada = new FileInputStream(file);
+        rep.load(entrada, senha);
+        entrada.close();
+        PublicKey chavePublica = rep.getCertificate("wayne enterprises, inc").getPublicKey();
+        System.out.println("Chave Pública encontrada!");
+        return chavePublica;
+    }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import javax.xml.bind.DatatypeConverter;
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.nio.file.Files;
 import java.security.*;
 import java.util.Arrays;
@@ -7,9 +9,9 @@ import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args) throws NoSuchAlgorithmException, SignatureException, InvalidKeyException, IOException {
+    public static void main(String[] args) throws Exception {
         Assinatura assinatura = new Assinatura();
-        File arquivo = new File("C:\\desenvolvimento\\POC\\src\\main\\java\\AFD.txt");
+        File arquivo = new File("src/main/java/AFD.txt");
 
         //byte[] mensagem = new byte[(int) arquivo.length()];
         byte[] mensagem = Files.readAllBytes(arquivo.toPath());
@@ -20,7 +22,6 @@ public class Main {
         System.out.println(new String(mensagem, "UTF-8"));
 
         PublicKey pubKey = assinatura.getPubKey();
-        System.out.println("pubKey : " + pubKey.toString());
 
         System.out.println("assinaturaDoc byte : " + Arrays.toString(assinaturaDoc));
         String assinaturaTxt = DatatypeConverter.printBase64Binary(assinaturaDoc);
@@ -28,8 +29,8 @@ public class Main {
 
         validaMensagem(pubKey, mensagem, assinaturaDoc);
 
-        FileWriter fw = new FileWriter( arquivo, true );
-        BufferedWriter bw = new BufferedWriter( fw );
+        FileWriter fw = new FileWriter(arquivo, true);
+        BufferedWriter bw = new BufferedWriter(fw);
         bw.newLine();
         bw.write(assinaturaTxt);
 
@@ -43,7 +44,7 @@ public class Main {
 
     public static void validaMensagem(PublicKey pubKey, byte[] mensagem, byte[] assinatura) throws
             NoSuchAlgorithmException, InvalidKeyException, SignatureException {
-        Signature clientSig = Signature.getInstance("DSA");
+        Signature clientSig = Signature.getInstance("MD5withRSA");
         clientSig.initVerify(pubKey);
         clientSig.update(mensagem);
 
